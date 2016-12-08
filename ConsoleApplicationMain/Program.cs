a68bf00ad42a04b378f8fe0faedc4d1271bf583a
NightWolf815 <orlovandry@mail.ru>
2016-12-08 14:49:34 +0300

    add console

---
+++ ConsoleApplicationMain/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationMain
+{
+    class Program
+    {
+        //string Menu = "1.Добавить\r\n2.Изменить\r\n3.Удалить";
+
+
+        static void Main(string[] args)
+        {
+            List<string[]> superlist = new List<string[]>();
+            string[] tempString = new string[5];
+            string y = "y";
+            while ("y" == y)
+            {
+                string Menu = "1.Добавить\r\n2.Изменить\r\n3.Удалить";
+                Console.WriteLine("Выберите вариант:");
+                Console.WriteLine(Menu);
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("\nВведите название проекта:");
+                            tempString[0] = Console.ReadLine();
+                            Console.WriteLine("Введите механизм хранения:");
+                            tempString[1] = Console.ReadLine();
+                            Console.WriteLine("Введите тип базы знаний:");
+                            tempString[2] = Console.ReadLine();
+                            Console.WriteLine("Введите механизм вывода:");
+                            tempString[3] = Console.ReadLine();
+                            Console.WriteLine("Введите правило:");
+                            tempString[4] = Console.ReadLine();
+                            superlist.Add(tempString);
+                            Console.WriteLine(Menu);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("\nВыберите проект:");
+                            int sch = 1;
+                            foreach (string[] a in superlist)
+                            {
+                                Console.WriteLine(sch + ") " + a[0]);
+                                sch++;
+                            }
+
+                            try
+                            {
+                                int a1 = int.Parse(Console.ReadLine()) - 1;
+                                tempString = superlist[a1];
+                                Console.WriteLine("\nВведите название проекта:");
+                                tempString[0] = Console.ReadLine();
+                                Console.WriteLine("Введите механизм хранения:");
+                                tempString[1] = Console.ReadLine();
+                                Console.WriteLine("Введите тип базы знаний:");
+                                tempString[2] = Console.ReadLine();
+                                Console.WriteLine("Введите механизм вывода:");
+                                tempString[3] = Console.ReadLine();
+                                Console.WriteLine("Введите правило:");
+                                tempString[4] = Console.ReadLine();
+                                superlist[a1] = tempString;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("\nЧто-то пошло не так!");
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("\nВыберите проект:");
+                            int sch = 1;
+                            foreach (string[] a in superlist)
+                            {
+                                Console.WriteLine(sch + ") " + a[0]);
+                                sch++;
+                            }
+                            try
+                            {
+                                int a1 = int.Parse(Console.ReadLine()) - 1;
+                                superlist.RemoveAt(a1);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("\nЧто-то пошло не так!");
+                            }
+                            break;
+                        }
+
+                }
+                /*tempString = null;*/
+                tempString = new string[5];
+                Console.WriteLine("\nПродолжить?[y/n]");
+                y = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplicationMain/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleApplicationMain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Windows")]
+[assembly: AssemblyProduct("ConsoleApplicationMain")]
+[assembly: AssemblyCopyright("Copyright © Windows 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("945ddc40-ff92-45c0-bc11-722e8ae13580")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
